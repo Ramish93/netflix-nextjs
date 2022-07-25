47f39d07cf8cab2685609f471aad63fec0692e57
Ramish Hassan <ramishshah93@gmail.com>
2022-07-26 00:53:59 +0200

    added types file typings.d.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ import Banner from "../components/Banner";
 import Header from "../components/Header";
 import requests from "../utils/request";
 
-const Home: NextPage = () => {
+const Home = ({ netflixOriginals }) => {
+  console.log(netflixOriginals);
+
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
