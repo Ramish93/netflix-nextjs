dd94d2ff98919656cd67e969c67f07e7bc3ad9a8
Ramish Hassan <ramishshah93@gmail.com>
2022-07-31 14:31:29 +0200

    added arrows and title to the row compoenent with css

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import { Movie } from "../typings";
 
 interface Props {
@@ -6,7 +7,15 @@ interface Props {
 }
 
 function Row({ title, movies }: Props) {
-  return <div>Row</div>;
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2>{title}</h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon className="absolute top-0 left-2 bottom-0 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 " />
+        <ChevronRightIcon className="absolute top-0 left-2 bottom-0 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 " />
+      </div>
+    </div>
+  );
 }
 
 export default Row;
