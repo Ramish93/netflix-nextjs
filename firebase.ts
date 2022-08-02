ed53cda9631da75927a9f82491ec0c886336ee6d
Ramish Hassan <ramishshah93@gmail.com>
2022-08-02 22:43:01 +0200

    created and added firebase config for DB and auth

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtbCaK136uMr0wQpVyUwWIT0rV95G21Aw",
+  authDomain: "netflix-clone-456e8.firebaseapp.com",
+  projectId: "netflix-clone-456e8",
+  storageBucket: "netflix-clone-456e8.appspot.com",
+  messagingSenderId: "64273027070",
+  appId: "1:64273027070:web:2771147e477a5c9c913aaf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
