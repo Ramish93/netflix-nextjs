57e2e79dba52b23987e5f4acf03fd27d1d6e6a66
Ramish Hassan <ramishshah93@gmail.com>
2022-08-04 15:01:15 +0200

    fixed error that was not returning the trailer in useeffect in model component, was using usestate instead of recoil state which actually had the movies/trailers

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,14 +1,39 @@
 import { useRecoilState, useRecoilValue } from "recoil";
-import { modalState } from "../atoms/modalAtom";
+import { modalState, movieState } from "../atoms/modalAtom";
 import MuiModal from "@mui/material/Modal";
 import { XIcon } from "@heroicons/react/solid";
+import { useEffect, useState } from "react";
 
 const Modal = () => {
   const [showModal, setShowModal] = useRecoilState(modalState);
+  const [movie, setMovie] = useRecoilState(movieState);
+  const [trailer, setTrailer] = useState();
+
+  useEffect(() => {
+    if (!movie) return;
+    const fetchMovie = async () => {
+      try {
+        const data = await fetch(
+          `https://api.themoviedb.org/3/${
+            movie?.media_type === "tv" ? "tv" : "movie"
+          }/${movie?.id}?api_key=${
+            process.env.NEXT_PUBLIC_API_KEY
+          }&language=en-US&append_to_response=videos`
+        ).then((responce) => responce.json());
+
+        setTrailer(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchMovie();
+  }, [movie]);
+  console.log(trailer);
 
   const handleClose = () => {
     setShowModal(false);
   };
+
   return (
     <MuiModal open={showModal} onClose={handleClose}>
       <>
@@ -18,6 +43,8 @@ const Modal = () => {
         >
           <XIcon className="h-6 w-6" />
         </button>
+
+        <div></div>
       </>
     </MuiModal>
   );
